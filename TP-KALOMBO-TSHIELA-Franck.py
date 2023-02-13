ada694264203c287096ece2cd2fb724fdba63822
Francky-Shadow <franckytshiela@gmail.com>
2023-02-13 04:42:19 +0100

    TP-KALOMBO-TSHIELA-Franck updated

--- TP-KALOMBO-TSHIELA-Franck.py
+++ TP-KALOMBO-TSHIELA-Franck.py
@@ -31,10 +31,20 @@ def and_functwithdecallage(t1,t2):
     return decallagedroite
 
 def chiffrement(t1,t2):
-    N=input("Veuillez entrer un bloc de taille 8 bits \n")
-    bloc=list(N)
-    permut=input("Veuillez saisir PI la clé de permutation \n")
-    pi=list(permut)
+    bloc=[]
+    pi=[]
+    N=[]
+    shortkey=[]
+    while(len(bloc)<8):
+        N=input("Veuillez entrer un bloc de taille 8 bits \n")
+        bloc=list(N)
+    while(len(pi)<8):
+        permut=input("Veuillez saisir PI la clé de permutation pi de taille 8 \n")
+        pi=list(permut)
+    while(len(shortkey)<4):
+        shortkey=input("Veuillez saisir la clé de permutation P de taille 4 \n")
+        sk=list(shortkey)
+    skserie=[eval(t) for t in sk]
     key=[eval(i) for i in bloc]
     yapermut=[eval(j) for j in pi]
     kw=[]
@@ -44,29 +54,45 @@ def chiffrement(t1,t2):
     go=kw[0:4]
     do=kw[4:8]
 
+    serializer1=[]
+    for s in range(0,4):
+        eof=skserie[s]
+        serializer1.append(go[eof])
+   
     d1=[]
     for j in range(0,4):
-        d1.append(go[j]^xor_funct_withdecallage(t1, t2)[j])
+        d1.append(serializer1[j]^xor_funct_withdecallage(t1, t2)[j])
 
     g1=[]
     for j in range(0,4):
-        g1.append(do[j]^(do[j]|xor_funct_withdecallage(t1, t2)[j]))
-
+        g1.append(do[j]^(go[j]|xor_funct_withdecallage(t1, t2)[j]))
+ 
+    serializer2=[]
+    for s in range(0,4):
+        eof=skserie[s]
+        serializer2.append(g1[eof])
     d2=[]
     for j in range(0,4):
-        d2.append(g1[j]^and_functwithdecallage(t1,t2)[j])
+        d2.append(serializer2[j]^and_functwithdecallage(t1,t2)[j])
     g2=[]
     for j in range(0,4):
-        g2.append(d2[j]^(do[j]|xor_funct_withdecallage(t1, t2)[j]))
-    return d1
-bloc=0
-pi=0
-N=0
-"""while(len(bloc)<8 or len(pi)<8):"""
-N=input("Veuillez entrer un bloc de taille 8 bits \n")
-bloc=list(N)
-permut=input("Veuillez saisir PI la clé de permutation \n")
-pi=list(permut)
+        g2.append(d1[j]^(g1[j]|xor_funct_withdecallage(t1, t2)[j]))
+    message=[]
+    for m in range (0,4):
+        message.append(g2[m])
+    for t in range (0,4):
+        message.append(d2[t])
+    print("LE MESSAGE CHIFFRE EST \n")
+    return message
+bloc=[]
+pi=[]
+N=[]
+while(len(bloc)<8):
+    N=input("Veuillez entrer un bloc de taille 8 bits \n")
+    bloc=list(N)
+while(len(pi)<8):
+    permut=input("Veuillez saisir PI la clé de permutation de taille 8 \n")
+    pi=list(permut)
 
 
 key=[eval(i) for i in bloc]
@@ -82,4 +108,5 @@ k2=kw[4:8]
 print("LES CLES GENEREES SONT \n")
 print(xor_funct_withdecallage(k1, k2))
 print(and_functwithdecallage(k1, k2))
+
 print(chiffrement(k1, k2))
